f8a9fdc358d5524411d00a6e823b0dca97ad1a38
Cin1983! <cinnymaria@gmail.com>
2020-12-15 23:20:30 +0100

    15 december 2020 start

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# weather_sensor_app
+React with Redux 15 december 2020
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import React from "react-dom";
+
+const App = () => {
+  return <div>Hey you, doe je best he!</div>;
+};
+
+ReactDOM.render(<App />,
+    document.querySelector("#root")
+)
+
+export default App;
