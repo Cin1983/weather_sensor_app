87edb8161f27e392efe3b4fbd441c5e42fbd4297
Cin1983! <cinnymaria@gmail.com>
2020-12-16 00:05:17 +0100

    15 december 2020 start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
-import React from "react-dom";
+import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hey you, doe je best he!</div>;
-};
+    
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        error => console.log(error)
+    );
+    
+    return <div>Hey you, doe je best he!</div>
+     };
+
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.render(<App />,
     document.querySelector("#root")
---
+++ src/seasonDisplay.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const seasonDisplay = () => {
+    return (
+      
+        <div> Season Display</div>
+    )
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default seasonDisplay
