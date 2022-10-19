2b13abf5a047c400f6b3ba0714401e86aa11b093
Cinny <69380613+Cin1983@users.noreply.github.com>
2022-10-19 21:35:56 +0200

    some error

--- src/index.js
+++ src/index.js
@@ -4,63 +4,43 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
 
+class App extends React.Component {
+  state = { latitude: null, errorMessage: "" };
 
-
-class App extends React.Component{
-
- 
-    state = { latitude: null, errorMessage: "" };
-
-
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ latitude: position.coords.latitude }),
-            error => this.setState({ errorMessage: error.message })
-   );
-
-}
-    renderContent() {
-       
-         if (this.state.errorMessage && !this.state.latitude){
-                return <div> Error: {this.state.errorMessage}</div>
-        }
-        if (!this.state.erroMessage && this.state.latitude) {
-            return <SeasonDisplay latitude = {this.state.latitude}/>
-        }
-        return <Spinner message="Please accept location request..." />;
-           
-                
-       
-}
-    
-    
-    
-// REACT WANTS US TO DEFINE RENDER!!
-    render() {
-        return (
-            <div className="border-red">
-                {this.renderContent()}
-            </div>
-        );
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ latitude: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
+    );
+  }
+  renderContent() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div> Error: {this.state.errorMessage}</div>;
     }
+    if (!this.state.erroMessage && this.state.latitude) {
+      return <SeasonDisplay latitude={this.state.latitude} />;
+    }
+    return <Spinner message="Please accept location request..." />;
+  }
+
+  // REACT WANTS US TO DEFINE RENDER!!
+  render() {
+    return <div className="border-red">{this.renderContent()}</div>;
+  }
 }
 
-
-
-ReactDOM.render(<App />,
-    document.querySelector("#root")
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
 
 export default App;
 
 // functional component
 // const App = () => {
-    
+
 //     window.navigator.geolocation.getCurrentPosition(
 //         position => console.log(position),
 //         error => console.log(error)
 //     );
-    
+
 //     return <div>Latitude:</div>
 //      };
 
